700905feb07b03d367e388329f700ab6e7871423
Hirisci <www.alan92@icloud.com>
2022-09-16 14:26:04 +0200

    Snack 2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿// snack 1
+﻿// Snack 1
 // Due int e trova il maggiore
+
+Console.WriteLine("Snack 1");
+Console.WriteLine();
 
 Console.WriteLine("Inserisci due numeri:");
 Console.Write("Inserisci Primo numero: ");
@@ -10,4 +13,18 @@ double numB = Convert.ToDouble(Console.ReadLine());
 Console.Write($"Il numero maggiore é ");
 Console.WriteLine(numA > numB ? numA : numB);
 
+// Snack 2
+// Parola piú lunga
+
+Console.WriteLine("Snack 2");
+Console.WriteLine();
+
+Console.WriteLine("Inserisci due Parole:");
+Console.Write("Inserisci Primo Parola: ");
+string wordA = Console.ReadLine();
+Console.Write("Inserisci Primo Parola: ");
+string wordB = Console.ReadLine();
+
+Console.Write($"La parola piú lunga è ");
+Console.WriteLine(wordA.Length > wordB.Length ? wordA : wordB);
 
