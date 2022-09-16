9c19132c5f1becbf1430bef25728172de595453c
Hirisci <www.alan92@icloud.com>
2022-09-16 14:41:58 +0200

    snack 4

--- Program.cs
+++ Program.cs
@@ -27,13 +27,30 @@
 //Console.WriteLine(wordA.Length > wordB.Length ? wordA : wordB);
 
 
-Console.WriteLine("Snack 3");
+//Console.WriteLine("Snack 3");
+//// somma di 10 input utente
+//Console.WriteLine();
+
+//int somma = 0;
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.Write("Inserisci un numero: ");
+//    somma += Convert.ToInt32(Console.ReadLine());
+//}
+//Console.WriteLine($"La somma dei numeri inseriti é: {somma}");
+
+
+Console.WriteLine("Snack 4");
+// somma e media da 2 a 10
 Console.WriteLine();
 
 int somma = 0;
-for (int i = 0; i < 10; i++)
+int numeri= 0
+for (int i = 2; i <= 10; i++)
 {
-    Console.Write("Inserisci un numero: ");
-    somma += Convert.ToInt32(Console.ReadLine());
+    somma+= i;
+    numeri++;
 }
-Console.WriteLine($"La somma dei numeri inseriti é: {somma}");
+Console.WriteLine($"La somma dei numeri da 2 a 10 é {somma}");
+Console.WriteLine($"La somma dei numeri da 2 a 10 é {somma/numeri}");
+
