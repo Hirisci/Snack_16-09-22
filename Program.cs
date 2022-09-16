e68489a050b22d3bc965e2e2088ca931d345ff94
Hirisci <www.alan92@icloud.com>
2022-09-16 14:17:56 +0200

    Esercizio 1 finito

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// snack 1
+// Due int e trova il maggiore
+
+Console.WriteLine("Inserisci due numeri:");
+Console.Write("Inserisci Primo numero: ");
+double numA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Inserisci Primo numero: ");
+double numB = Convert.ToDouble(Console.ReadLine());
+
+Console.Write($"Il numero maggiore é ");
+Console.WriteLine(numA > numB ? numA : numB);
+
 
